a5bd9d6746f2691f84fea2b1153617b48b64243e
tsukinousagi18 <148684666+tsukinousagi18@users.noreply.github.com>
2026-04-01 19:04:45 +0530

    Added "tests" folder with the specified framework

---
+++ tests/Qiskit/Bell State Preparation/phiplus.py
@@ -0,0 +1,17 @@
+import pprint
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+from qiskit.circuit.library import XGate, YGate, ZGate, HGate, SGate, TGate
+
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.cx(0,1)
+
+qc.measure_all()
+simulator = AerSimulator()
+qc = transpile(qc, simulator)
+compiled_circuit = simulator.run(qc, shots=16384)
+result = compiled_circuit.result()
+counts = result.get_counts(qc)
+probs = {state: count / 16384 for state, count in counts.items()}
+pprint.pprint(probs)
