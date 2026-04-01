c73975bf4eb95186c73a374e160427bc6988ef1c
tsukinousagi18 <148684666+tsukinousagi18@users.noreply.github.com>
2026-04-01 19:11:50 +0530

    Added psiminus.py for Bell state preparation

---
+++ tests/Qiskit/Bell State Preparation/psiminus.py
@@ -0,0 +1,20 @@
+import pprint
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import AerSimulator
+from qiskit.circuit.library import XGate, YGate, ZGate, HGate, SGate, TGate
+
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.x(1)
+qc.z(0)
+qc.z(1)
+qc.cx(0,1)
+
+qc.measure_all()
+simulator = AerSimulator()
+qc = transpile(qc, simulator)
+compiled_circuit = simulator.run(qc, shots=16384)
+result = compiled_circuit.result()
+counts = result.get_counts(qc)
+probs = {state: count / 16384 for state, count in counts.items()}
+pprint.pprint(probs)
