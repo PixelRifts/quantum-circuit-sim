359e81f94e37269e21790e910195967f9226d0af
Mayank935-code <148620862+Mayank935-code@users.noreply.github.com>
2026-04-03 20:26:14 +0530

    Update GHZ_State.py

--- tests/Cirq/GHZ_State.py
+++ tests/Cirq/GHZ_State.py
@@ -3,12 +3,13 @@ import pprint
 
 q0, q1, q2 = cirq.LineQubit.range(3)
 
-circuit = cirq.Circuit(
-    cirq.H(q0),
-    cirq.CNOT(q0, q1),
-    cirq.CNOT(q1, q2),
-    cirq.measure(q0, q1, q2, key='m')
-)
+circuit = cirq.Circuit()
+
+circuit.append(cirq.H(q0))
+circuit.append(cirq.CNOT(q0, q1))
+circuit.append(cirq.CNOT(q1, q2))
+
+circuit.append(cirq.measure(q0, q1, q2, key='m'))
 
 sim = cirq.Simulator()
 result = sim.run(circuit, repetitions=16384)
